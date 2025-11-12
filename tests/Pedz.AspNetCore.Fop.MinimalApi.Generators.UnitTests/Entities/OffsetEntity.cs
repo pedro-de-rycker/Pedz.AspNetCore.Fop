20e7f622a991b32875cdf134aba8a98645bf9528
De Rycker Pedro <pedro@de-rycker.com>
2025-11-12 22:52:32 +0100

    rewriting tests and activating them on the github actions pipeline

---
+++ tests/Pedz.AspNetCore.Fop.MinimalApi.Generators.UnitTests/Entities/OffsetEntity.cs
@@ -0,0 +1,10 @@
+﻿using Pedz.AspNetCore.Fop.MinimalApi.Attributes;
+
+namespace Pedz.AspNetCore.Fop.MinimalApi.Generators.UnitTests.Entities;
+
+[OffsetFop]
+public class OffsetEntity
+{
+    [FopProperty(IsSortable = true, IsFilterable = false)]
+    public Guid Id { get; set; }
+}
